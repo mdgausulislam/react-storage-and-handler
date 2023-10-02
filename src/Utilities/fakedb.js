e868b890414abf8323606a9d0b48cc7b682defcb
unknown <gausulislam5@gmail.com>
2023-10-02 23:33:42 +0600

    update section

---
+++ src/Utilities/fakedb.js
@@ -0,0 +1,15 @@
+// use local Storage to manage card data
+
+
+const addToDb=id=>{
+   const quantity=localStorage.getItem(id);
+   if(quantity){
+    console.log("already exits");
+    const newQuantity=parseInt(quantity)+1;
+    localStorage.setItem(id,newQuantity);
+   }else{
+    console.log("new item");
+    localStorage.setItem(id,1);
+   }
+}
+export{addToDb};
--- src/components/Cosmetic1/Cosmetic.js
+++ src/components/Cosmetic1/Cosmetic.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Cosmetic.css';
+import { addToDb } from '../../Utilities/fakedb';
 
 const Cosmetic = (props) => {
     const {name,price,id} = props.cosmetic;
     const addToCart = (id)=>{
-        console.log("clicked",id);
+        addToDb(id);
     }
 
     const addToCartWithParam=()=>{addToCart(id)}
