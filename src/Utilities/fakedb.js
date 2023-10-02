f6cb51393866b67f774defc469e91b7671c778a7
unknown <gausulislam5@gmail.com>
2023-10-03 00:22:33 +0600

    update local storage

--- src/Utilities/fakedb.js
+++ src/Utilities/fakedb.js
@@ -1,15 +1,28 @@
 // use local Storage to manage card data
 
 
-const addToDb=id=>{
-   const quantity=localStorage.getItem(id);
-   if(quantity){
-    console.log("already exits");
-    const newQuantity=parseInt(quantity)+1;
-    localStorage.setItem(id,newQuantity);
-   }else{
-    console.log("new item");
-    localStorage.setItem(id,1);
-   }
+const addToDb = id => {
+    let shoppingCart;
+
+    const storedCart = localStorage.getItem('Shopping-cart');
+    if (storedCart) {
+        shoppingCart = JSON.parse(storedCart);
+    } else {
+        shoppingCart = {};
+    }
+    const quantity = shoppingCart[id];
+    if (quantity) {
+        console.log("already exits");
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity;
+        // localStorage.setItem(id,newQuantity);
+    } else {
+        console.log("new item");
+        // localStorage.setItem(id,1);
+        shoppingCart[id] = 1;
+    }
+    localStorage.setItem('Shopping-cart', JSON.stringify(shoppingCart))
+
+
 }
-export{addToDb};
+export { addToDb };
